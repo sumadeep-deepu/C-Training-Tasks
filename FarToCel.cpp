1db6ea43c499d0474a0a22f03782c7dd2c6fa160
sumadeep-deepu <147999349+sumadeep-deepu@users.noreply.github.com>
2023-12-19 11:21:47 +0530

    Update FarToCel.cpp

--- FarToCel.cpp
+++ FarToCel.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-	float a;
+	float Ftemp;
 	cout<<"enter the Fahrenheit temperature : ";
-	cin>>a;
-	float b=(a-32)*5/9;
-	cout<<"the equivalent celius temperature is : "<<b<<"C"<<endl;
+	cin>>Ftemp;
+	float Ctemp=(Ftemp-32)*5/9;
+	cout<<"the equivalent celius temperature is : "<<Ctemp<<"C"<<endl;
 	return 0;
 
 }
