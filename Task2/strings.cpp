116531dd9055270847ecfad371b409850e4bd5f6
sumadeep-deepu <147999349+sumadeep-deepu@users.noreply.github.com>
2023-12-21 10:35:31 +0530

    Update strings.cpp

--- Task2/strings.cpp
+++ Task2/strings.cpp
@@ -2,7 +2,6 @@
 #include<string>
 #include<iostream>
 using namespace std;
-
 string reverse(string word){
 	string r;
 	for(int i = word.length() - 1;i >= 0;i--){
@@ -18,17 +17,15 @@ int main(){
 	string word;
 	for(char c:sentence)
 	{
-	   if(c==' ')
-	   {
-	    words.push_back(reverse(word));
-	    word="";
+	   if(c==' '){
+		   words.push_back(reverse(word));
+		   word="";
 	   }
 	   else{
 	   word+=c;
 	   }
-	   }
+	}
 	words.push_back(reverse(word));	
-	
 	for(int i=0;i<words.size();++i){
 		cout<<words[i]<<" ";
 	}
